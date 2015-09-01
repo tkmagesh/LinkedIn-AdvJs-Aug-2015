8816a72c6ac7004f354f5578fe40a757d8ab0071
tkmagesh <tkmagesh77@gmail.com>
2015-09-01 12:24:46 +0530

    spinner problem

---
+++ Day-02/02-Closures/spinner.js
@@ -0,0 +1,17 @@
+create an object and assign it to the variable "spinner"
+the object is expected to exhibit the following characterestics.
+
+var spinner = ....
+
+spinner.up() // => 1
+spinner.up() // => 2
+spinner.up() // => 3
+spinner.up() // => 4
+
+spinner.down() // => 3
+spinner.down() // => 2
+spinner.down() // => 1
+spinner.down() // => 0
+spinner.down() // => -1
+
+The variable used to the track the value should NOT be visible to the outside world
