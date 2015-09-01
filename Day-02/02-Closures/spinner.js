6fd1a5dc1d453be4f4524dba718289eeb22a781a
tkmagesh <tkmagesh77@gmail.com>
2015-09-01 14:28:36 +0530

    jQuery Intro

--- Day-02/02-Closures/spinner.js
+++ Day-02/02-Closures/spinner.js
@@ -1,7 +1,23 @@
+/*
 create an object and assign it to the variable "spinner"
 the object is expected to exhibit the following characterestics.
+*/
 
-var spinner = ....
+function getSpinner(){
+    var count = 0;
+    function increment(){
+        return ++count;
+    }
+    function decrement(){
+        return --count;
+    }
+    return {
+        up : increment,
+        down : decrement
+    }
+}
+
+var spinner = getSpinner();
 
 spinner.up() // => 1
 spinner.up() // => 2
@@ -14,4 +30,6 @@ spinner.down() // => 1
 spinner.down() // => 0
 spinner.down() // => -1
 
+/*
 The variable used to the track the value should NOT be visible to the outside world
+*/
